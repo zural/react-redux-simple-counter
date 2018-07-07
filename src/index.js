bae9bf67b84f4cb860cb98710eb069949ef309ee
BOSTON\sivaraj.v <zural143@gmail.com>
2018-07-07 12:32:11 +0530

    Asyn call promise updated

--- src/index.js
+++ src/index.js
@@ -3,15 +3,77 @@ import { render } from "react-dom";
 import { Provider, connect } from "react-redux";
 import { createStore } from "redux";
 
-/*
-Main STATE structure
-const state = {count: 0};
-*/
+// var promise = new Promise(function(resolve, reject) {
+//     resolve(true);
+//   })
+//   promise.then(bool => console.log('Bool is true'))
+
+// var promise1 = Promise.resolve(3);
+// var promise2 = 42;
+// var promise3 = new Promise(function(resolve, reject) {
+//   setTimeout(resolve, 100, 'foo');
+// });
+
+// Promise.all([promise1, promise2, promise3]).then(function(values) {
+//   console.log(values);
+// });
+// // expected output: Array [3, 42, "foo"]
+
+// const promises = [
+//     new Promise(resolve => setTimeout(resolve, 0, 1)),
+//     new Promise(resolve => setTimeout(resolve, 0, 2))
+//   ];
+//   Promise.all(promises)
+//     .then(data => {
+//       console.log("First handler", data);
+//       return data.map(entry => entry * 10);
+//     })
+//     .then(data => {
+//       console.log("Second handler", data);
+//     });
+
+
+// Promise.all([
+//     fetch("https://jsonplaceholder.typicode.com/posts/1").then(response=>response.json()),
+//     fetch("https://jsonplaceholder.typicode.com/posts/2"),
+//     fetch("https://jsonplaceholder.typicode.com/posts/3")
+//   ]).then((allResponses) => {
+//     const response1 = allResponses[0].body;
+//     const response2 = allResponses[1]
+//     const response3 = allResponses[2]
+//     console.log()
+//   }).catch((err) => {
+//       console.log(err);
+//   });
+
+
+// var apiRequest1 = fetch('https://jsonplaceholder.typicode.com/posts/1').then(function(response){
+//     return response.json()
+// });
+// var apiRequest2 = fetch('https://jsonplaceholder.typicode.com/posts/2').then(function(response){
+//     return response.json()
+// });
+// var combinedData = {"apiRequest1":{},"apiRequest2":{}};
+// Promise.all([apiRequest1,apiRequest2]).then(function(values){
+// combinedData["apiRequest1"] = values[0];
+// combinedData["apiRequest2"] = values[1];
+// return combinedData;
+// });
+
+// fetch('https://jsonplaceholder.typicode.com/posts/2').then(
+//   function(response){
+//      var val = response.json()
+//      return val;
+//     }
+// ).then(function(jsonData){
+//     jsonData;
+//     //handle json data processing here
+// });
+
 
 //STEP 1 - Create REDUCER and returns the new state object
 const userReducer = (state, action) => {
     state === undefined ? (state = { count: 0 }) : null; //Definition for beginning state value and its structure
-
     switch (action.type) {
         case "INCREMENT":
             return Object.assign({}, state, { count: state.count + 1 }); //Using non-mutating method
@@ -23,25 +85,19 @@ const userReducer = (state, action) => {
 };
 
 //STEP 2 - Create STORE
-const store = createStore(userReducer); //Redux method
-
+const store = createStore(
+    userReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); //Redux method
 //STEP 3 - Create MAIN CLASS and its methods with using dispatch(action)
 class Counter extends React.Component {
     render() {
         return (
             <div>
-                <h2>Simple React-Redux example - Counter (Part 2)</h2>
                 <div>
                     <button onClick={this.props.actionDecrease}>-</button>
                     <span>{this.props.count}</span>
                     <button onClick={this.props.actionIncrease}>+</button>
-                </div>
-                <div>
-                    <p>
-                        Everything in one place for it becomes easier to
-                        understand about how Redux state and React component communicate with each other.
-                    </p>
-                    <p>Using mapStateToProps and mapDispatchToProps</p>
                 </div>
             </div>
         );
